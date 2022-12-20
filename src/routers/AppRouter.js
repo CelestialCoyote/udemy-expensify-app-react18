a1415141bf116f95175ea09f843847bca6a203b3
Paul Stearns <celestialcoyote@gmail.com>
2022-12-20 11:54:47 -0800

    initial commit

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ExpenseDashboardPage from '../pages/ExpenseDashboardPage';
+import AddExpensePage from '../pages/AddExpensePage';
+import EditExpensePage from '../pages/EditExpensePage';
+import HelpPage from '../pages/HelpPage';
+import NotFoundPage from '../pages/NotFoundPage';
+import Header from '../components/Header';
+
+const AppRouter = () => (
+    <BrowserRouter>
+        <div>
+            <Header />
+            <Routes>
+                {/*<Route path="/" element={<ExpenseDashboardPage />} exact={true} />*/}
+                <Route path="/" element={<ExpenseDashboardPage />} />
+                <Route path="/create" element={<AddExpensePage />} />
+                <Route path="/edit/:id" element={<EditExpensePage />} />
+                <Route path="/help" element={<HelpPage />} />
+                <Route element={<NotFoundPage />} />
+            </Routes>
+        </div>
+    </BrowserRouter>
+);
+
+export default AppRouter;

00066f2639c7f0d8d11e0e3045db64165a282812
Paul Stearns <celestialcoyote@gmail.com>
2022-12-21 09:24:46 -0800

    Added some css to ExpenseListItem component.

--- src/components/ExpenseListItem/ExpenseListItem.js
+++ src/components/ExpenseListItem/ExpenseListItem.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './ExpenseListItem.css';
 
 
-const ExpenseListItem = ({ id, description, amount, createdAt }) => (
-    <div>
+const ExpenseListItem = ({ id, description, amount, note, createdAt }) => (
+    <div className="expense-list-item">
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
         <p>{amount} - {createdAt}</p>
+        <p>{note}</p>
     </div>
 );
 
